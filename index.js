f559a7c33d05818396db1a18417dbab6231bfced
Janaka Jayasuriya <Janaka Jayasuriya>
2017-01-24 15:19:56 +0800

    0.0.2

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 'use strict';
 
-/**
- * Initializes Firebase SDK
- * @param {enableLogging} boolean Enable logging
- * @return {firebase} Initialized firebase instance
- */
-module.exports = function (enableLogging = false) {
-	var admin = require('firebase-admin')
+const admin = require('firebase-admin')
 
-	// TODO Investigate whether this hack is safe to be deployed on Heroku
-	var privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 
-	admin.initializeApp({
-		credential: admin.credential.cert({
-			projectId: process.env.FIREBASE_PROJECT_ID,
-			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-			privateKey: privateKey
-		}),
-		databaseURL: process.env.FIREBASE_DB_URL
-	})
+admin.initializeApp({
+	credential: admin.credential.cert({
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+		privateKey: privateKey
+	}),
+	databaseURL: process.env.FIREBASE_DB_URL
+})
 
-	admin.database.enableLogging(enableLogging);
-
-	return admin
-};
+module.exports = admin
